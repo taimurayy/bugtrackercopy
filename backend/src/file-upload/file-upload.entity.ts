8dc8a0b30d9000512786b97f442f7a7c751ebdcf
TAIMUR <tkma560432@gmail.com>
2024-08-26 18:21:45 +0500

    cahnges

--- backend/src/file-upload/file-upload.entity.ts
+++ backend/src/file-upload/file-upload.entity.ts
@@ -1,6 +1,6 @@
 import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
 
-@Entity('FileUpload')
+@Entity('file_upload')
 export class FileUpload {
   @PrimaryGeneratedColumn()
   id: number = 0; // Initialize with a default value
