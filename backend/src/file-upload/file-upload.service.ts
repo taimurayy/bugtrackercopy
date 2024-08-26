f5efd5b55a1d07b9fea6237f8b5ca96b220cd235
TAIMUR <tkma560432@gmail.com>
2024-08-26 17:31:54 +0500

    file_uploading

--- backend/src/file-upload/file-upload.service.ts
+++ backend/src/file-upload/file-upload.service.ts
@@ -1,6 +1,6 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { Repository, EntityManager } from 'typeorm';
 import { FileUpload } from './file-upload.entity'; // Adjust the path to your FileUpload entity
 
 @Injectable()
@@ -8,16 +8,30 @@ export class FileUploadsService {
   constructor(
     @InjectRepository(FileUpload)
     private readonly fileUploadRepository: Repository<FileUpload>,
+    private readonly entityManager: EntityManager, // Ensure EntityManager is injected
   ) {}
 
   async saveFile(bugReportId: number, file: Express.Multer.File) {
-    // Create a new FileUpload instance and save it to the database
-    const fileUpload = new FileUpload();
-    fileUpload.id = bugReportId;
-    fileUpload.filename = file.originalname; // Use originalname for the original filename
-    fileUpload.path = file.path;
-    fileUpload.bugReportId = bugReportId;
-    console.log(fileUpload);
-    return this.fileUploadRepository.save(fileUpload);
+    const query = `
+      INSERT INTO file_upload (id, filename, path, bugReportId, uploaded_at)
+      VALUES ($1, $2, $3, $4, NOW())
+    `;
+
+    const parameters = [
+      bugReportId,
+      file.originalname,
+      file.path,
+      bugReportId,
+    ];
+
+    await this.entityManager.query(query, parameters);
+
+    return {
+      id: bugReportId,
+      filename: file.originalname,
+      path: file.path,
+      bugReportId: bugReportId,
+      uploadedAt: new Date(), // You can return additional fields if needed
+    };
   }
 }
